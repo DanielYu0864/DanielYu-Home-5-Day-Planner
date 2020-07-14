9f8e1d0e21a52e794de30a29bb00373525093a3e
Daniel.Yu <adam741963@gmail.com>
2020-07-14 13:01:58 -0700

    clear btn

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 $(document).ready(function(){// tells engine to load html and css first.
-    // display the date and time
+    // array for #hours and hours
+    var arrayHoursId = ["#hour6", "#hour7", "#hour8", "#hour9", "#hour10", "#hour11", "#hour12", "#hour13", "#hour14", "#hour15", "#hour16", "#hour17", "#hour18", "#hour19", "#hour20"]
+    var NotHashHours = [];
+    // var
+    var getItemTime;
+    // display the date and time by [Moment.js]
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
+
+    // event listener to clear all the plan
+    $(".clean").on("click", function() {
+        $(this).val(localStorage.clear(getItemTime));
+        $(this).val(window.location.reload());
+    })
+
     // sbmit function for save value to localStorage
     $(".saveBtn").on("click", function() {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     });
-    // array for #hours and hours
-    var arrayHoursId = ["#hour6", "#hour7", "#hour8", "#hour9", "#hour10", "#hour11", "#hour12", "#hour13", "#hour14", "#hour15", "#hour16", "#hour17", "#hour18", "#hour19", "#hour20"]
-    var NotHashHours = [];
+
+
     // founction for remove hash tag
+
     function removedHashTag() {
         for (i = 0; i <= arrayHoursId.length - 1; i++) {
             NotHashHours[i] = arrayHoursId[i].split("#").pop();
         }
     }
     removedHashTag();
-
     // function for looping $().val(localStorage.getItem());
     function loopForTime() {
         for (i = 1; i < 15; i++) {
             var hourDescribe = arrayHoursId[i];
             var description = " .description";
-            var getItemTime = NotHashHours[i];
+            getItemTime = NotHashHours[i];
             $(hourDescribe + description).val(localStorage.getItem(getItemTime));
         }
         // output like this
@@ -31,9 +42,9 @@ $(document).ready(function(){// tells engine to load html and css first.
     }
     loopForTime();
 
-
+    // function to tracking hours and change the colors
     function trackHours() {
-        //current number of hours.
+        //current number of hours by [Moment.js]
         var currentTime = moment().hour();
 
         $(".time-block").each(function() {
